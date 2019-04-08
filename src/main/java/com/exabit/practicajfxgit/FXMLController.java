4b1edc5b69631685fd09c0b8090a3e36e34f7c97
isaiasmar <isaiasmar@prg01-pc08>
2019-04-08 17:01:07 -0400

    Nombre agregado en primer Button

--- src/main/java/com/exabit/practicajfxgit/FXMLController.java
+++ src/main/java/com/exabit/practicajfxgit/FXMLController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -34,6 +35,9 @@ public class FXMLController implements Initializable {
 
     @FXML
     private void onActionBtn1(ActionEvent event) {
+        Alert al=new Alert(Alert.AlertType.INFORMATION);
+        al.setContentText("Isaias Marmolejo");
+        al.show();
     }
 
     @FXML
