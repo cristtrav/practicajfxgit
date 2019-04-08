b0fa4b090e47197b79115ee0e8055e84a23bb5ee
traver <traver@traver-note>
2019-04-08 16:30:41 -0400

    Commit inicial

---
+++ src/main/java/com/exabit/practicajfxgit/FXMLController.java
@@ -0,0 +1,66 @@
+package com.exabit.practicajfxgit;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btnTest;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onActionBtn1(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtn2(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtn3(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtn4(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtn5(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtn6(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtn7(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnTest(ActionEvent event) {
+    }
+}
