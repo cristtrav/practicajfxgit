7fb8914e8e43017472cce5737deb77e777d361c5
elton <elton@prg01-pc5>
2019-04-15 14:06:57 -0400

    Mensaje de Alerta bt4

--- src/main/java/com/exabit/practicajfxgit/FXMLController.java
+++ src/main/java/com/exabit/practicajfxgit/FXMLController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -46,10 +47,15 @@ public class FXMLController implements Initializable {
 
     @FXML
     private void onActionBtn4(ActionEvent event) {
+          Alert a=new Alert(Alert.AlertType.INFORMATION);
+       a.setContentText("Este boton fue hecho por forastero");
+       a.show();
     }
+    
 
     @FXML
     private void onActionBtn5(ActionEvent event) {
+     
     }
 
     @FXML
